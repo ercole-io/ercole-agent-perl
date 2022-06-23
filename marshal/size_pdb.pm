1f5c7708c91e57cf1c07b71c9abd6f8342ed009f
cbiglioli <cbiglioli@gmail.com>
2022-06-23 18:16:23 +0200

    Wrong data DB space utilization

---
+++ marshal/size_pdb.pm
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+# Copyright (c) 2022 Sorint.lab S.p.A.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package marshal;
+
+use strict;
+use warnings;
+use diagnostics;
+use lib "./marshal";
+use common;
+
+# Sizepdb returns PDB segmentsSize, datafileSize and allocable values from the output of the size_pdb fetcher command.
+
+sub Sizepdb {
+    no warnings 'uninitialized';
+    my $cmdOutput = shift;
+    my $segmentsSizePDB;
+    my $datafileSizePDB;
+    my $allocablePDB;
+
+    for my $c (split /\n/, $cmdOutput) {
+        my $line = $c;
+        
+        my @values = split (/\|\|\|/, $line);
+        if ( scalar @values ne 3 ) {
+            next;
+        }
+
+        my ($segmentsSize, $datafileSize, $allocable) = split (/\|\|\|/, $line);
+
+        $segmentsSizePDB=parseNumber(trim($segmentsSize));
+        $datafileSizePDB=parseNumber(trim($datafileSize));
+        $allocablePDB=parseNumber(trim($allocable));
+    }
+
+    return $segmentsSizePDB, $datafileSizePDB, $allocablePDB;
+}
+
+1;
